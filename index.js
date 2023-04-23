79682a96f4a7fd62be619cc89de57f64315d0e93
nikita-laad <nikitalaad1997@gmail.com>
2023-04-23 10:39:27 -0700

    Add package

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const connectToMongo = require('./db');
 const express = require('express');
 connectToMongo();
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT;
-
+app.use(express.json());
+app.use(cors())
 app.use(express.json());
 //Routes
 app.use('/api', require('./routes/auth'));
